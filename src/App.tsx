8ff8d1210be401282ed96841bd6f612d2690645c
Kanatbek <kanatbekabad@gmail.com>
2026-03-07 22:24:59 +0500

    feat: integrate movie API with loading and error states

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
+import { useEffect, useState } from "react"
 import Header from "./components/Header"
 import SearchBar from "./components/SearchBar"
 import MovieGrid from "./components/MovieGrid"
-import { movies } from "./services/movies"
+import type { Movie } from "./types/movie"
 
-function App() {
+export default function App() {
+  const [movies, setMovies] = useState<Movie[]>([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState<string | null>(null)
+
+  useEffect(() => {
+    fetch("https://www.omdbapi.com/?s=batman&apikey=564727fa")
+      .then(res => res.json())
+      .then(data => {
+        const mappedMovies = data.Search.map((movie: any) => ({
+          id: movie.imdbID,
+          title: movie.Title,
+          year: Number(movie.Year),
+          poster: movie.Poster
+        }))
+
+        setMovies(mappedMovies)
+        setLoading(false)
+      })
+      .catch(() => {
+        setError("Failed to load movies")
+        setLoading(false)
+      })
+  }, [])
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>{error}</p>
+
   return (
     <div>
       <Header />
@@ -12,5 +40,3 @@ function App() {
     </div>
   )
 }
-
-export default App
